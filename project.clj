6123799de441c75b0dd4a5e6f5ae4f95839a44ee
mtumilowicz <michal.tumilowicz01@gmail.com>
2022-06-05 15:47:28 +0200

    project.clj

--- project.clj
+++ project.clj
@@ -4,5 +4,4 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.0"]
-                 [org.clojure/test.check "1.1.1"]]
-  :repl-options {:init-ns clojure-concurrency-stm-workshop.core})
+                 [org.clojure/test.check "1.1.1"]])
